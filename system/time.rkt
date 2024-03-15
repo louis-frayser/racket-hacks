c6cda6f8cfcf4e028856be5b9cd47f36348f2ca1
Louis Frayser <louis.frayser@gmail.com>
2024-03-15 02:27:53 -0700

    tstamp: Added switch to select HTML timstamp output

--- system/time.rkt
+++ system/time.rkt
@@ -5,16 +5,19 @@
 (define (~0 n) (~a #:width 2 #:left-pad-string "0" #:align 'right n))
 (require (only-in racket/date current-date))
 
-
-(define (tstamp)
-  (define date  (current-date))         
-  (format "~a-~a-~a ~a:~a"
+;; tstamp :: IO String
+(define (tstamp #:for-html? (ht? #f))
+  (define date  (current-date))
+  (define-values (~f sep ) (if ht? (values ~0 "T") (values ~a " "))) 
+  (format "~a-~a-~a~a~a:~a"
           (date-year date)
-          (~0 (date-month date))
-          (date-day date)
-          (date-hour date)
-          (date-minute date)))
+          (~0 (date-month  date))
+          (~f (date-day    date))
+          sep
+          (~f (date-hour   date))
+          (~f (date-minute date))))
 
+;; to-cs :: String->String
 (define (to-cs hhmmsscs-str) ;;; HH:MM:SS.SS => centisecs
   (let* ((lst (map string->number (string-split hhmmsscs-str  #px"[:.]")))
          (hh (first lst))
@@ -23,6 +26,7 @@
          (cs  (fourth lst)))
     (+ cs  (* 100 ss)  (* 100 60 mm)  (* 100 60 60 hh))))
 
+;; cs-to-hms-str :: String->String
 (define (cs-to-hms-str css) ;;; Centisec to HH:MM:S.s (string)
   (let*-values (( (hh cs1) (quotient/remainder css 360000 ))
                 ( (mm cs2)  (quotient/remainder cs1 6000))
