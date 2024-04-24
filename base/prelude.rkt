d7a4625ba6416d38e771b21b1ed9380339feb721
Louis Frayser <louis.frayser@gmail.com>
2024-04-24 12:47:39 -0700

    Basic haskell-like sequence functions

---
+++ base/prelude.rkt
@@ -0,0 +1,32 @@
+#lang racket ;; prelude.rkt
+;; functions from Haskell Prelude
+
+(require (only-in srfi/1 (take scm:take) (drop scm:drop) (take-right scm:take-right)))
+
+(provide drop take take-end)
+
+(define (take n xs)
+  (if (> n  (length xs))
+      xs
+      (scm:take xs n)))
+
+(define (take-end n xs)
+  (if (> n (length xs))
+      xs
+      (scm:take-right xs n)))
+
+;; Haskell drop
+(define (drop n xs)
+  (if (>= n (length xs))
+      '()
+      (scm:drop xs n)))
+
+#|
+;;; Tests
+(map (lambda(x)(take x '(1 2 3 4 5 6 7 8 9 10 11)))
+     '(10 11 12))
+(define xs11 '(1 2 3 4 5 6 7 8 9 10 11))
+  
+(map (lambda(n)(take-end n xs11))
+     '(10 11 12))
+|#
