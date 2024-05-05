ffc8f8eb26b3c315fd698e0349f6e2284e2561c3
Louis Frayser <louis.frayser@gmail.com>
2024-05-05 10:05:46 -0700

    Made system declare deps: racko setup --fix-pkg-deps

--- info.rkt
+++ info.rkt
@@ -3,7 +3,11 @@
 (define version "0.1")
 (define collection "racket-hacks")
 (define name "racket-hacks")
-(define deps '("base" ))
+(define deps '("db-lib"
+               "srfi-lib"
+               "srfi-lite-lib"
+               "base" ))
 
 #;(define build-deps '("racket-doc" "scribble-lib"))
 #;(define scribblings '(("scribblings/racket-hacks.scrbl" ())))
+(define build-deps '("debug"))
